ee48cc1ee9aed74d4dc5cfe31e4d4e5dd594d927
georgif0x <georgifox@hotmail.com>
2023-09-28 16:55:10 +0000

    Primer comit

---
+++ clase/tema2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduccion tema 2 en clase</title>
+</head>
+<body>
+    <?php
+        echo"hola mundo";
+        print "hola mundo";
+        //para arrays print_r
+    ?>
+
+
+</body>
+</html>
